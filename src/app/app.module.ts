e8dde4feb3a97d9a5bf8d0ae8491a4c18fd981e5
Aravindha-samy <aravindhasamychs2017@gmail.com>
2024-07-03 15:59:19 +0530

    Footer Component  Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     LayoutComponent
   ],
   imports: [
