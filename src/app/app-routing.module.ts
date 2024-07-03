045a60c767c0fdeb907811271942579c2eeb5b21
santhoshkotti <126395649+santhoshkotti@users.noreply.github.com>
2024-07-03 14:37:36 +0530

    infrastructure defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'layout', component: LayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
