6495690f57f34c151fe8ad1336163cfe12f77c6a
Cshiva2603 <shivasundaramc@gmail.com>
2024-07-05 12:56:51 +0530

    contract view updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ContractviewComponent } from './components/contractview/contractview.component';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LayoutComponent,
+    ContractviewComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/interfaces/options.ts
@@ -0,0 +1,44 @@
+import { DecimalPipe, Time } from '@angular/common';
+
+export interface Options {
+  Id: number;
+  Key: string;
+  Value: string;
+}
+
+export interface Users {
+  Id: number;
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  HashPassword: string;
+}
+
+export interface Contracts {
+  Id: number;
+  UniqueNo: number;
+  Version: number;
+  Current: boolean;
+  BusinessOwnerId: number;
+  CounterParty: string;
+  Account: string;
+  Client: string;
+  Project: string;
+  CounterPartyOwner: string;
+  LegalOwnerId: number;
+  SigningDate: Date;
+  StartingDate: Date;
+  TerminationDate: Date;
+  RenewalDateFlagOff: boolean;
+  Value: number;
+  Jurisdiction: string;
+  LiabilitiesCap: number;
+
+  Notes: string;
+  OpenIssues: string;
+
+  Link: string;
+  AutoRenewal: boolean;
+  ContractCode: number;
+  Filename: string;
+}
